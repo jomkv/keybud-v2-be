c52046890d3f5e9669e4ee3bee92cea6bfc13f0f
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-09-28 04:21:38 +0800

    feat: structured responses

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(4040);
 }
 bootstrap();
