ba158a20b5ac63f073e67a39fc5fc6796c073709
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-10-04 23:16:57 +0800

    feat: centralized req logic and updated env variables

--- src/shared/env-variables.ts
+++ src/shared/env-variables.ts
@@ -8,6 +8,26 @@ class EnvironmentVariables {
   get isDev(): boolean {
     return !this.isProd;
   }
+
+  get clientId(): string {
+    return process.env.GOOGLE_CLIENT_ID;
+  }
+
+  get clientSecret(): string {
+    return process.env.GOOGLE_CLIENT_SECRET;
+  }
+
+  get baseUrl(): string {
+    return process.env.BASE_URL;
+  }
+
+  get redisHost(): string {
+    return process.env.REDIS_HOST;
+  }
+
+  get redisPort(): number {
+    return process.env.REDIS_PORT as unknown as number;
+  }
 }
 
 export default new EnvironmentVariables();
---
+++ src/shared/services/request.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Scope } from '@nestjs/common';
+import { TokenPayload } from '../types/auth';
+
+@Injectable({ scope: Scope.REQUEST })
+export class RequestService {
+  private userPayload: TokenPayload;
+  private sessionNonce: string | null = null;
+
+  setUserPayload(payload: TokenPayload) {
+    this.userPayload = payload;
+  }
+
+  getUserPayload() {
+    return this.userPayload;
+  }
+
+  setSessionNonce(nonce: string | null): void {
+    this.sessionNonce = nonce;
+  }
+
+  getSessionNonce(): string | null {
+    return this.sessionNonce;
+  }
+}
