5d611bbbfd3bb136d172d06a8b89f4e8da713aa8
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-10-05 20:28:37 +0800

    chore: add /api prefix and install more dependencies for auth

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import { AllExceptionFilter } from './shared/filter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Globals
+  app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionFilter());
+
   await app.listen(4040);
 }
 bootstrap();
