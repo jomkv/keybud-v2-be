ac01e9e9867cfa3daa44de6a1b5716eb97d9c485
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-10-12 13:57:26 +0800

    refactor: rename filter to filters (plural)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
-import { AllExceptionFilter } from './shared/filter/exception.filter';
+import { AllExceptionFilter } from './shared/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
