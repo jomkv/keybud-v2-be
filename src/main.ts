a2fa03c8dfa2382b9b841c09d6885a098c8620db
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-10-16 11:26:25 +0800

    fix: add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 import { AllExceptionFilter } from './shared/filters/exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
 
   // Globals
   app.setGlobalPrefix('api');
