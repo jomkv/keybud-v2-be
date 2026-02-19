a50c0d053cc91131a4b3b66b91b86ec9f82fc3ae
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-02-19 13:52:57 +0800

    fix: add cascade on conversation member's conversation. with corresponding migration

---
+++ prisma/migrations/20260219055213_add_conversation_cascade_at_conversation_member/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "ConversationMember" DROP CONSTRAINT "ConversationMember_conversationId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "ConversationMember" ADD CONSTRAINT "ConversationMember_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
